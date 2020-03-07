faeb9c6d1e11e5f9b9b8a60a1a1dad7fba955528
Dessaleng Ewart <dewart11@gmail.com>
2020-03-07 14:44:17 +0100

    Just added Minimum

--- src/Minimum.java
+++ src/Minimum.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Minimum{
     public static void main(String [] args){
-        int[] digi={0,1,2,3,4,5,6,7,8,9};
+        int[] digi={9,8,7,6,5,4,3,2,1,0};
         Arrays.sort(digi);
         System.out.println("Minimum = " + digi[0]);
 
