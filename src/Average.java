fa0c6ef6f32846441ffa598c900715e6dd077d38
Dessaleng Ewart <dewart11@gmail.com>
2020-03-07 12:02:43 +0100

    Just added Maximum

---
+++ src/Average.java
@@ -0,0 +1,16 @@
+public class Average{
+    public static void main(String [] args){
+        double[] values = {0,1,2,3,4,5,6,7,8,9};
+        double total = 0;
+        for (double element : values) {
+            total += element;
+
+        }
+        System.out.println(total);
+        double average = 0;
+        if (values.length > 0){
+            average = total / values.length;
+        }
+        System.out.println(average);
+    }
+}
